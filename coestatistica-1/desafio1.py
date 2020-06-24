12a1cb11a4af51f48d0b57b278cef59b2d6b3e1a
Helio Alves <heleralvess@gmail.com>
2020-06-24 19:03:50 -0300

    Semana 3: Análise Exploratória

---
+++ coestatistica-1/desafio1.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+# carregando o dataset
+df = pd.read_csv("desafio1.csv")
+
+# visualizando os 5 primeiros registros para conhecermos nossos dados
+df.head()
+
+# verificando os tipos de dados encontrados
+df.dtypes
+
+# visualizando a variável que devemos realizar o desafio
+df["pontuacao_credito"]
+
+# visualizando os estados únicos do dataset
+df["estado_residencia"].unique()
+
+# Criando uma função que irá retornar todos os cálculos
+
+
+def stats(df):
+    return {"moda": df.mode(), "mediana": df.median(), "media": df.mean(), "desvio_padrao": df.std()}
+
+
+# Agrupando para relacionar estado_residencia e pontuacao_credito
+# Aggregate => podemos retornar todos os cálculos de uma só vez utilizando a função aggregate e armazendo em um novo Dataframe
+df_final = df.groupby("estado_residencia")[
+    "pontuacao_credito"].apply(stats).unstack()
+
+# Precisamos renomear as colunas para ir de acordo com o desafio
+df_final.columns = ["moda", "mediana", "media", "desvio_padrao"]
+print(df_final)
+
+# Fazendo a conversão para .json
+df_final.to_json("submission.json")
