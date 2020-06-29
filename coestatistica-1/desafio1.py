1e8c9fb15b036b873199b822d207195183bdfec4
Helio Alves <heleralvess@gmail.com>
2020-06-29 15:21:34 -0300

    Semana 3: Solução utilizando Função Aggregate

--- coestatistica-1/desafio1.py
+++ coestatistica-1/desafio1.py
@@ -18,14 +18,22 @@ df["estado_residencia"].unique()
 # Criando uma função que irá retornar todos os cálculos
 
 
-def stats(df):
-    return {"moda": df.mode()[0], "mediana": df.median(), "media": df.mean(), "desvio_padrao": df.std()}
+# def stats(df):
+#     return {"moda": df.mode()[0], "mediana": df.median(), "media": df.mean(), "desvio_padrao": df.std()}
 
 
-# Agrupando para relacionar estado_residencia e pontuacao_credito
-# Apply => recebe uma função como parâmetro que se aplica para cada valor
-df_final = df.groupby("estado_residencia")[
-    "pontuacao_credito"].apply(stats).unstack()
+# # Agrupando para relacionar estado_residencia e pontuacao_credito
+# # Apply => recebe uma função como parâmetro que se aplica para cada valor
+# df_final = df.groupby("estado_residencia")[
+#     "pontuacao_credito"].apply(stats).unstack()
+
+# Utilizando a função aggregate para aplicar as operações de uma vez
+df_final = df.groupby("estado_residencia")["pontuacao_credito"].agg(
+    ["mean", "median", pd.Series.mode, "std"])
+print(df_final)
+
+df_final.rename(columns={"mean": "media", "median": "mediana",
+                         "mode": "moda", "std": "desvio_padrao"}, inplace=True)
 
 # Fazendo a conversão para .json
 df_final.to_json("submission.json", orient="index")
